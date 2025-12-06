8bf4545ab0558717b80d571bb37703c551cb8b7a
DenisShagalin <shagalindenis@gmail.com>
2025-12-06 09:32:44 +0200

    corrected meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import "./globals.css";
 
-import { ConfigProvider } from 'antd';
-import { NextIntlClientProvider } from 'next-intl';
-import { getLocale, getMessages } from 'next-intl/server';
+import { ConfigProvider } from "antd";
+import { NextIntlClientProvider } from "next-intl";
+import { getLocale, getMessages } from "next-intl/server";
 
 import { Background } from "./components/background";
 import { Section } from "./components/section";
-import { Footer } from './components/footer';
+import { Footer } from "./components/footer";
 import { MainLinks } from "./components/main-links";
 
 export default async function RootLayout({
@@ -21,12 +21,23 @@ export default async function RootLayout({
     <html lang={locale}>
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
-        <title>2D & 3D Grundrisse online ab 25 € | 3dplan.online</title>
+
+        <title>2D & 3D Grundrisse online | 3dplan.online</title>
+        <meta
+          name="description"
+          content="Digitale 2D/3D-Grundrisse. Wohnflächenberechnung, Exposé-Grafiken, Digitalisierung alter Pläne. Hochwertige & professionell Grundriss visualisierung - 3dplan.online"
+        />
+        <meta
+          name="keywords"
+          content="2D Grundrisse online bestellen, 3D Grundrisse erstellen lassen, Grundriss Service, Wohnflächenberechnung online, Grundriss für Exposé, Digitalisierung Baupläne, alte Grundrisse digitalisieren, Grundriss Visualisierung, Immobilienpräsentation Grundriss, Architektenplan digital"
+        />
+
+        {/* <title>2D & 3D Grundrisse online ab 25 € | 3dplan.online</title>
         <meta name='description' content='Digitale 2D/3D-Grundrisse ab 25 € online bestellen. Wohnflächenberechnung, Exposé-Grafiken, Digitalisierung alter Pläne. Hochwertige & professionell Grundriss visualisierung - 3dplan.online' />
-        <meta name='keywords' content='2D Grundrisse online bestellen, 3D Grundrisse erstellen lassen, Grundriss Service ab 25 Euro, Wohnflächenberechnung online, Grundriss für Exposé, Digitalisierung Baupläne, alte Grundrisse digitalisieren, Grundriss Visualisierung, Immobilienpräsentation Grundriss, Architektenplan digital' />
+        <meta name='keywords' content='2D Grundrisse online bestellen, 3D Grundrisse erstellen lassen, Grundriss Service ab 25 Euro, Wohnflächenberechnung online, Grundriss für Exposé, Digitalisierung Baupläne, alte Grundrisse digitalisieren, Grundriss Visualisierung, Immobilienpräsentation Grundriss, Architektenplan digital' /> */}
       </head>
       <body>
-      {children}
+        {children}
         {/* <ConfigProvider
           theme={{
             components: {
