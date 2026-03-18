4b0622b60e7e3471a26e302accf7867b375886b5
DenisShagalin <shagalindenis@gmail.com>
2026-03-18 10:13:53 +0200

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ConfigProvider } from "antd";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
+import Script from "next/script";
 
 import { Background } from "./components/background";
 import { Section } from "./components/section";
@@ -28,6 +29,19 @@ export default async function RootLayout({
           name="description"
           content="Grundriss für Immobilien Exposé. Grundriss für Immobilienverkauf. Immobilien Grundriss Service. Grundriss für Immobilienanzeige. Grundriss für Makler. Grundriss Service online. Grundriss erstellen lassen. Grundriss digitalisieren. 3D Grundriss Wohnung. 2D Grundriss mit Maßen. Grundriss für Immobilien Exposé. Grundriss erstellen Preis. Immobilien Visualisierung Grundriss. Professioneller Grundriss Immobilien"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18022087039"
+        ></script>
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            // @ts-ignore
+            window.dataLayer.push(arguments)
+          }
+          gtag('js', new Date());
+          gtag('config', 'AW-18022087039');`}
+        </Script>
       </head>
       <body>
         <ConfigProvider
